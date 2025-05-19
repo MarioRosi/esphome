96df5c2ffe69c9f82d0fb88cfd225c3cae9a3f99
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-19 15:41:10 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/__init__.py
+++ esphome/components/rollershutter/__init__.py
@@ -145,14 +145,14 @@ async def to_code(config):
                 rlssundowner[CONF_RLS_SD_UH],
                 rlssundowner[CONF_RLS_SD_UM],
             ))
-    if CONF_RLS_ALLSH in rlsRoot:
-        cg.add(var.SetButtons(
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPU],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPD],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPH],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_MAS],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_SLV],
-        ))
+    # if CONF_RLS_ALLSH in rlsRoot:
+    #     cg.add(var.SetButtons(
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPU],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPD],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPH],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_MAS],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_SLV],
+    #     ))
     # for rlshutter in rlsRoot.get(CONF_RLS_SHUTTERS, []):
     #     cg.add(var.AddShutter(
     #         rlshutter[CONF_ID].__str__(),
