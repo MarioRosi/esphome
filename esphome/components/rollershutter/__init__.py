326baca0d4c7a08e790d20f3ef4f5668c06066cd
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-19 15:44:24 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/__init__.py
+++ esphome/components/rollershutter/__init__.py
@@ -127,24 +127,24 @@ async def to_code(config):
             rlstime[CONF_RLS_TIMES_MSD],
             rlstime[CONF_RLS_TIMES_MSGD]
         ))
-    for rlsgroup in rlsRoot.get(CONF_RLS_GROUPS, []):
-        rlssundowner = rlsgroup.get(CONF_RLS_SD)
-        if CONF_RLS_SD_OFF in rlssundowner:
-            cg.add(var.AddGroup(
-                rlsgroup[CONF_ID].__str__(),
-                rlsgroup[CONF_NAME]
-            ))
-        else:
-            cg.add(var.AddGroup(
-                rlsgroup[CONF_ID].__str__(),
-                rlsgroup[CONF_NAME],
-                rlssundowner[CONF_RLS_SD_MF],
-                rlssundowner[CONF_RLS_SD_MT],
-                rlssundowner[CONF_RLS_SD_GH],
-                rlssundowner[CONF_RLS_SD_GM],
-                rlssundowner[CONF_RLS_SD_UH],
-                rlssundowner[CONF_RLS_SD_UM],
-            ))
+    # for rlsgroup in rlsRoot.get(CONF_RLS_GROUPS, []):
+    #     rlssundowner = rlsgroup.get(CONF_RLS_SD)
+    #     if CONF_RLS_SD_OFF in rlssundowner:
+    #         cg.add(var.AddGroup(
+    #             rlsgroup[CONF_ID].__str__(),
+    #             rlsgroup[CONF_NAME]
+    #         ))
+    #     else:
+    #         cg.add(var.AddGroup(
+    #             rlsgroup[CONF_ID].__str__(),
+    #             rlsgroup[CONF_NAME],
+    #             rlssundowner[CONF_RLS_SD_MF],
+    #             rlssundowner[CONF_RLS_SD_MT],
+    #             rlssundowner[CONF_RLS_SD_GH],
+    #             rlssundowner[CONF_RLS_SD_GM],
+    #             rlssundowner[CONF_RLS_SD_UH],
+    #             rlssundowner[CONF_RLS_SD_UM],
+    #         ))
     # if CONF_RLS_ALLSH in rlsRoot:
     #     cg.add(var.SetButtons(
     #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPU],
