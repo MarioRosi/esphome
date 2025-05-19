916d25786ad636ae6e099fde872c7087219c30f5
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-19 15:21:36 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/__init__.py
+++ esphome/components/rollershutter/__init__.py
@@ -116,43 +116,43 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
 )
 
 async def to_code(config):
-    rlsRoot = config
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    cg.add(var.SetIdAndName(config[CONF_ID].__str__(), config[CONF_NAME]))
-    for rlstime in rlsRoot.get(CONF_RLS_TIMES, []):
-        cg.add(var.AddTime(
-            rlstime[CONF_ID].__str__(),
-            rlstime[CONF_RLS_TIMES_MSU],
-            rlstime[CONF_RLS_TIMES_MSD],
-            rlstime[CONF_RLS_TIMES_MSGD]
-        ))
-    for rlsgroup in rlsRoot.get(CONF_RLS_GROUPS, []):
-        rlssundowner = rlsgroup.get(CONF_RLS_SD)
-        if CONF_RLS_SD_OFF in rlssundowner:
-            cg.add(var.AddGroup(
-                rlsgroup[CONF_ID].__str__(),
-                rlsgroup[CONF_NAME]
-            ))
-        else:
-            cg.add(var.AddGroup(
-                rlsgroup[CONF_ID].__str__(),
-                rlsgroup[CONF_NAME],
-                rlssundowner[CONF_RLS_SD_MF],
-                rlssundowner[CONF_RLS_SD_MT],
-                rlssundowner[CONF_RLS_SD_GH],
-                rlssundowner[CONF_RLS_SD_GM],
-                rlssundowner[CONF_RLS_SD_UH],
-                rlssundowner[CONF_RLS_SD_UM],
-            ))
-    if CONF_RLS_ALLSH in rlsRoot:
-        cg.add(var.SetButtons(
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPU],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPD],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPH],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_MAS],
-            rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_SLV],
-        ))
+    # rlsRoot = config
+    # var = cg.new_Pvariable(config[CONF_ID])
+    # await cg.register_component(var, config)
+    # cg.add(var.SetIdAndName(config[CONF_ID].__str__(), config[CONF_NAME]))
+    # for rlstime in rlsRoot.get(CONF_RLS_TIMES, []):
+    #     cg.add(var.AddTime(
+    #         rlstime[CONF_ID].__str__(),
+    #         rlstime[CONF_RLS_TIMES_MSU],
+    #         rlstime[CONF_RLS_TIMES_MSD],
+    #         rlstime[CONF_RLS_TIMES_MSGD]
+    #     ))
+    # for rlsgroup in rlsRoot.get(CONF_RLS_GROUPS, []):
+    #     rlssundowner = rlsgroup.get(CONF_RLS_SD)
+    #     if CONF_RLS_SD_OFF in rlssundowner:
+    #         cg.add(var.AddGroup(
+    #             rlsgroup[CONF_ID].__str__(),
+    #             rlsgroup[CONF_NAME]
+    #         ))
+    #     else:
+    #         cg.add(var.AddGroup(
+    #             rlsgroup[CONF_ID].__str__(),
+    #             rlsgroup[CONF_NAME],
+    #             rlssundowner[CONF_RLS_SD_MF],
+    #             rlssundowner[CONF_RLS_SD_MT],
+    #             rlssundowner[CONF_RLS_SD_GH],
+    #             rlssundowner[CONF_RLS_SD_GM],
+    #             rlssundowner[CONF_RLS_SD_UH],
+    #             rlssundowner[CONF_RLS_SD_UM],
+    #         ))
+    # if CONF_RLS_ALLSH in rlsRoot:
+    #     cg.add(var.SetButtons(
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPU],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPD],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_IPH],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_MAS],
+    #         rlsRoot[CONF_RLS_ALLSH][CONF_RLS_ALLSH_SLV],
+    #     ))
     # for rlshutter in rlsRoot.get(CONF_RLS_SHUTTERS, []):
     #     cg.add(var.AddShutter(
     #         rlshutter[CONF_ID].__str__(),
