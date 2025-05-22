1d1491a190968732ac1b56bfeadbe54d9a3c31e7
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-22 22:52:56 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/rollershuttercomponent.cpp
+++ esphome/components/rollershutter/rollershuttercomponent.cpp
@@ -96,8 +96,6 @@ void RollerShutterComponent::AddGroup(const char* id, const char* name,
   RL_SunDowner *sd = new RL_SunDowner(monthFrom, monthTo, gapHour, gapMinute, upHoure, upMinute);
   ESP_LOGD(TAG, "AddGroup new SD");
   RL_Group *item = new RL_Group(id, name, sd);
-  
-  ESP_LOGD(TAG, " Free Mem %d", system_get_free_heap_size());
   ESP_LOGD(TAG, "AddGroup new GRP %s", item->name.c_str());
   this->groups->push_back(item); 
   ESP_LOGD(TAG, "AddGroup mit SD id = %s", id);
