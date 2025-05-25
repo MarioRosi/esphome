b013d591d4f8e403a9a13bf69fc9662239b7e7ff
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-25 22:47:00 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/rollershuttercomponent.cpp
+++ esphome/components/rollershutter/rollershuttercomponent.cpp
@@ -95,9 +95,9 @@ void RollerShutterComponent::AddGroup(const std::string &id, const std::string &
   RL_SunDowner *sd = new RL_SunDowner(monthFrom, monthTo, gapHour, gapMinute, upHoure, upMinute);
   ESP_LOGD(TAG, "AddGroup new SD");
   RL_Group *item = new RL_Group(id, name, sd);
-  ESP_LOGD(TAG, "AddGroup new GRP %s", item->name.c_str());
+  //ESP_LOGD(TAG, "AddGroup new GRP %s", item->name.c_str());
   this->groups->push_back(item); 
-  ESP_LOGD(TAG, "AddGroup mit SD id = %s", id);
+  //ESP_LOGD(TAG, "AddGroup mit SD id = %s", id);
 }
 
 /// @brief Fügt eine Gruppe ohne / mit deaktiviertem Sundowner der Liste hinzu
