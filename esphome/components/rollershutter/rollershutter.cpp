3625bed1fe35badc506735560747cc7870fe7349
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-26 19:44:56 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/rollershutter.cpp
+++ esphome/components/rollershutter/rollershutter.cpp
@@ -379,6 +379,7 @@ binary_sensor::BinarySensor *RollerShutter::getBinarySensorById(const std::strin
   binary_sensor::BinarySensor * result = nullptr;
   ESP_LOGD(TAG, "getBinarySensorById in");
   std::vector<binary_sensor::BinarySensor *> sensors = App.get_binary_sensors();
+  ESP_LOGD(TAG, "getBinarySensorById sonsors.Count=%d", sensors.size());
   /*
   for (int idx = 0; idx < sensors.size(); idx++) {
     binary_sensor::BinarySensor *binSesorComponent = sensors.at(idx);
