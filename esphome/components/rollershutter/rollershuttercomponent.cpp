b5171a5ac912f99e6c2e8d36b7e30888bec97c37
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-26 20:28:18 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/rollershuttercomponent.cpp
+++ esphome/components/rollershutter/rollershuttercomponent.cpp
@@ -244,8 +244,7 @@ void RollerShutterComponent::InitialRun() {
 
 /// @brief onSetup
 void RollerShutterComponent::setup() {
-  ESP_LOGD(TAG, "Setup RSC '%s'", this->myName.c_str());  
-  /*
+  ESP_LOGD(TAG, "Setup RSC '%s'", this->myName.c_str());    
   if (this->allBtnIsMaster) {
     if (std::strlen(this->btnUpId.c_str()) > 1) {
       if (this->btnDownId.compare("remote") != 0) {
