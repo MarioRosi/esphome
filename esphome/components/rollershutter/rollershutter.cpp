e91e5aba6185e4b44cdc45501ae8dc63143fbf5d
Mario Rosenbohm <mario.rosenbohm@web.de>
2025-05-28 16:42:59 +0200

    ... machen das es geht ;-)

--- esphome/components/rollershutter/rollershutter.cpp
+++ esphome/components/rollershutter/rollershutter.cpp
@@ -269,16 +269,18 @@ void RollerShutter::CheckTimerStop() {
 void RollerShutter::CheckButtons() {
   if (hasSetup) {
     if (this->btnUp->has_state())
-    {
-      ESP_LOGD(TAG, "BTN UP is Press for %s", this->myId.c_str());
+    {      
       this->btnUpIsPress = this->btnUp->state;
+      if (this->btnUpIsPress)
+        ESP_LOGD(TAG, "BTN UP is Press for %s", this->myId.c_str());
     }
     else
       this->btnUpIsPress = false;
     if (this->btnDown->has_state())
-    {
-      ESP_LOGD(TAG, "BTN DOWN is Press for %s", this->myId.c_str());
+    {      
       this->btnDownIsPress = this->btnDown->state;
+      if (this->btnDownIsPress)
+        ESP_LOGD(TAG, "BTN DOWN is Press for %s", this->myId.c_str());
     }
     else
       this->btnDownIsPress = false;
